c3027b1cb7e5d35492507afa8d62bff0b8fc24f9
Félix Dorn <github@felixdorn.fr>
2022-01-11 18:19:24 +0100

    experimenting with workflows

---
+++ util/workflow.go
@@ -0,0 +1,46 @@
+package workflows
+
+import (
+	"errors"
+)
+
+type Action func(...interface{}) ([]interface{}, error)
+
+var (
+	ErrSkipAction    = errors.New("skip action")
+	ErrInvalidAction = errors.New("invalid action")
+)
+
+type Workflow []interface{}
+
+func (w Workflow) Run(initialArgs ...interface{}) error {
+	var args []interface{}
+	var err error
+
+	for k, action := range w {
+		switch action := action.(type) {
+		case Action:
+			args, err = action(args...)
+			if err == ErrSkipAction {
+				continue
+			}
+
+			if err != nil {
+				return err
+			}
+		case Workflow:
+			err = action.Run(args...)
+			if err != nil {
+				return err
+			}
+		default:
+			if len(w) == k+1 { // last action
+				return ErrInvalidAction
+			}
+
+			args = append(args, action)
+		}
+
+		return nil
+	}
+}
